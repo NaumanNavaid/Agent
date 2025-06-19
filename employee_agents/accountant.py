8b91420f7a3953b715a41df4c59ea8566f755d1d
NaumanNavaid <161620542+NaumanNavaid@users.noreply.github.com>
2025-06-19 21:25:57 +0500

    changes

---
+++ employee_agents/accountant.py
@@ -0,0 +1,9 @@
+from conf import Model
+from agents import Agent
+
+
+accountant = Agent(
+    name="Ahmed raza dahrolia",
+    instructions="You are a helpful assistant that can help with accounting tasks. Your name is  ahmed raza dahrolia and you are a employee of the user and you are an qualified CA.",
+    model=Model,
+)
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from conf import Model
-from agents import Agent, Runner
+from employee_agents.accountant import accountant
+from agents import Runner
 import asyncio
 
-accountant = Agent(
-    name="Ahmed raza dahrolia",
-    instructions="You are a helpful assistant that can help with accounting tasks. Your name is  ahmed raza dahrolia and you are a employee of the user and you are an qualified CA.",
-    model=Model,
-)
+
 
 async def main():
-    response = await Runner.run(accountant, "Tell me the total fee structure of CA in Pakistan?")
+    
+    response = await Runner.run(accountant, input="Tell me the total fee structure of CA in Pakistan?")
+   
     print(response)
 
 asyncio.run(main())
